860808fee99999910fd5b6f1714da3adfad7941a
Marcel Zielinski <marcel.zielinski@capgemini.com>
2026-03-13 15:29:54 +0100

    Add button prop validation via props-types library

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Button from "./Button";
+
+function App() {
+    return <div className="App">
+        <h1>App</h1>
+        <div>
+            <Button success>Click there too</Button>
+        </div>
+        <div>
+            <Button>Hide ads</Button>
+        </div>
+        <div>
+            <Button></Button>
+        </div>
+        <div>
+            <Button></Button>
+        </div>
+        <div>
+            <Button></Button>
+        </div>
+    </div>
+}
+
+export default App;
