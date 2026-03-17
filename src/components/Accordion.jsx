061ee83a51eef07edb3c181a0684173f7eb0d76a
Marcel Zielinski <marcel.zielinski@capgemini.com>
2026-03-17 10:24:54 +0100

    New version of accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -2,7 +2,16 @@ import { useState } from "react";
 import {GoChevronLeft,GoChevronDown} from "react-icons/go";
 
 function Accordion({ items }) {
-    const [expandedIndex, setExpandedIndex] = useState(2);
+    const [expandedIndex, setExpandedIndex] = useState(-1);
+
+    const handleClick = (index) => {
+        setExpandedIndex((current) => {
+            if (current === index) {
+                return -1;
+            } else {
+                return index;
+            }});
+    }
 
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
@@ -13,7 +22,7 @@ function Accordion({ items }) {
 
         return (
             <div key={item.id}>
-                <div className="justify-between flex p-3 bg-grey-50 border-b items-center cursor-pointer" onClick={()=>setExpandedIndex(index)}>{item.label}{icon}</div>
+                <div className="justify-between flex p-3 bg-grey-50 border-b items-center cursor-pointer" onClick={()=>handleClick(index)}>{item.label}{icon}</div>
                 {content}
             </div>
         );
