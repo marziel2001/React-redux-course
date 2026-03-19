4ea2923b2f83a9bc7fad2dae0988166e80c7fa45
Marcel Zielinski <marcel.zielinski@capgemini.com>
2026-03-19 14:53:09 +0100

    Add sorting

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,5 +1,7 @@
 import { func } from "prop-types";
-import Table from "../components/Table";
+// import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
+
 
 function TablePage()
 {
@@ -11,16 +13,18 @@ function TablePage()
     ];
 
     const config = [
-        { label: 'Fruit',
-            render: (fruit) => fruit.name
+        {   label: 'Name',
+            render: (fruit) => fruit.name,
+            sortValue: (fruit) => fruit.name
         },
-        { label: 'Color',
+        {   label: 'Color',
             render: (fruit) => (
                 <div className={`p-3 m-2 ${fruit.color}`}></div>
             )
         },
-        { label: 'Score',
-            render: (fruit) => fruit.score
+        {   label: 'Score',
+            render: (fruit) => fruit.score,
+            sortValue: (fruit) => fruit.score
         },
     ];
 
@@ -30,7 +34,7 @@ function TablePage()
 
     return (
         <div>
-            <Table data={data} config={config} keyFn={keyFn} />
+            <SortableTable data={data} config={config} keyFn={keyFn} />
         </div>
     )
 }
