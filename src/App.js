595924ed91257de14a276046b1436ca74c5ee08c
Marcel Zielinski <marcel.zielinski@capgemini.com>
2026-03-30 14:44:38 +0200

    Setup project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import UsersList from "./components/UsersList";
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <UsersList>
+
+      </UsersList>
+    </div>
+  );
+}
+
+export default App;
