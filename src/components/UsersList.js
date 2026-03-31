2a1afec5ad48901a7a3d66d5072539b8ac8810f7
Marcel Zielinski <marcel.zielinski@capgemini.com>
2026-03-31 12:33:32 +0200

    Create loading boxes animation

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,29 +1,38 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
+import Skeleton from "./Skeleton";
 
 function UsersList() {
   const dispatch = useDispatch();
 
   const { isLoading, data, error } = useSelector((state) => {
     return state.users;
-  })
+  });
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, []);
 
   if (isLoading) {
-
-    return <div>Loading...</div>
+    return <Skeleton times={6} className="h-10 w-full" />;
   }
 
-  if (error)
-  {
-    return <div>Error</div>
+  if (error) {
+    return <div>Error</div>;
   }
 
-  return <div>{data.length}</div>
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return <div>{renderedUsers}</div>;
 }
 
 export default UsersList;
