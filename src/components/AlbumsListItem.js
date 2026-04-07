2a59f32a17d0c3531cf5155dacade79f933a9a70
Marcel Zielinski <marcel.zielinski@capgemini.com>
2026-04-07 11:42:09 +0200

    Add photos deletion

--- src/components/AlbumsListItem.js
+++ src/components/AlbumsListItem.js
@@ -2,6 +2,8 @@ import { GoTrashcan } from "react-icons/go";
 import Button from "./Button";
 import ExpandablePanel from "./ExpandablePanel";
 import { useRemoveAlbumMutation } from "../store";
+import PhotosListItem from "./PhotosListItem";
+import PhotosList from "./PhotosList";
 
 function AlbumsListItem({ album }) {
   const [removeAlbum, results] = useRemoveAlbumMutation();
@@ -21,7 +23,7 @@ function AlbumsListItem({ album }) {
 
   return (
     <ExpandablePanel key={album.id} header={header}>
-      List of the photos in the album
+      <PhotosList album={album} />
     </ExpandablePanel>
   );
 }
