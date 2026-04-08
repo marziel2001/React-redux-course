786ead6d3390c4f0b8b9a9aaf2153383fae223a9
Marcel Zielinski <marcel.zielinski@capgemini.com>
2026-04-08 15:09:04 +0200

    Implement programmatic routing example

---
+++ registry/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+import SearchInput from "./SearchInput";
+
+export default function Header() {
+  return <div>
+    <Link to={"/"}>Npm Registry</Link>
+    <SearchInput></SearchInput>
+  </div>
+}
